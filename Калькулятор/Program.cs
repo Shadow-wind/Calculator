af27db9ca79245ab29dcc7cc2e8a546f31d6947a
Shadow-wind <59884654+Shadow-wind@users.noreply.github.com>
2020-04-22 15:05:54 +0400

    Добавьте файлы проекта.

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
